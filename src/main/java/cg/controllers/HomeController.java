fc0e5ee1bb4aef752ea60e06953032b216e8bb5e
HP <langthanhsangls@gmail.com>
2022-02-23 16:52:36 +0700

    anh sang add

---
+++ src/main/java/cg/controllers/HomeController.java
@@ -0,0 +1,127 @@
+package cg.controllers;
+
+import cg.model.Product;
+import cg.service.IProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.util.FileCopyUtils;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+
+@Controller
+@RequestMapping("/product")
+@PropertySource("classpath:upload_file.properties")
+public class HomeController {
+
+    @Autowired
+    private IProductService productService;
+
+//    @ModelAttribute("products")
+//    public Iterable<Product> products(){
+//        return productService.getAllProduct();
+//    }
+
+    @Value("D:\\CODEGYM\\code\\bai5\\minitest1\\CRUD-DB-OneTable\\src\\main\\webapp\\image\\ava.jpg")
+
+    @GetMapping
+    public ModelAndView showProducts() {
+        ModelAndView modelAndView = new ModelAndView("list");
+        ArrayList<Product> products = productService.getAllProduct();
+        if (products.isEmpty()) {
+            modelAndView.addObject("message", "No products!");
+            modelAndView.addObject("color", "red");
+        }
+        modelAndView.addObject("products", products);
+        return modelAndView;
+    }
+
+    @GetMapping("/delete/{id}")
+    public ModelAndView deleteProduct(@PathVariable("id") int id) {
+        ModelAndView modelAndView = new ModelAndView("list");
+        Product product = productService.deleteProduct(id);
+        if (product == null) {
+            modelAndView.addObject("message", "Id invalid!");
+            modelAndView.addObject("color", "red");
+        }
+        ArrayList<Product> products = productService.getAllProduct();
+        modelAndView.addObject("products", products);
+        return modelAndView;
+    }
+
+    @GetMapping("/view/{id}")
+    public ModelAndView showDetail(@PathVariable("id") int id) {
+        ModelAndView modelAndView = new ModelAndView("detail");
+        Product product = productService.getProduct(id);
+        if (product != null) {
+            modelAndView.addObject("product", product);
+        } else {
+            modelAndView.addObject("message", "Id invalid!");
+        }
+        return modelAndView;
+    }
+
+    @GetMapping("/create")
+    public ModelAndView createProduct(Model model) {
+        ModelAndView modelAndView = new ModelAndView("create");
+        model.addAttribute("product", new Product());
+        return modelAndView;
+    }
+
+    @PostMapping
+    public ModelAndView create(@ModelAttribute Product product) {
+        ModelAndView modelAndView = new ModelAndView("create");
+        Product productCreate = productService.saveProduct(product);
+        if (productCreate != null) {
+            modelAndView.addObject("message", "Create successfully!");
+        }
+        return modelAndView;
+    }
+
+    @GetMapping("/edit/{id}")
+    public ModelAndView editProduct(@PathVariable("id") int id) {
+        ModelAndView modelAndView = new ModelAndView("edit");
+        Product product = productService.getProduct(id);
+        if (product != null) {
+            modelAndView.addObject("product", product);
+        } else {
+            modelAndView.addObject("message", "Id invalid!");
+        }
+        return modelAndView;
+    }
+
+    @PostMapping("/{id}")
+    public ModelAndView edit(@ModelAttribute Product product, @PathVariable int id) {
+        ModelAndView modelAndView = new ModelAndView("edit");
+        product.setId(id);
+        Product productEdit = productService.saveProduct(product);
+        if (productEdit != null) {
+            modelAndView.addObject("message", "Update successfully!");
+        }
+        return modelAndView;
+    }
+
+    @PostMapping("/save")
+    public ModelAndView saveProduct(@ModelAttribute Form form) {
+        MultipartFile multipartFile = pri.getImage();
+        String fileName = multipartFile.getOriginalFilename();
+        try {
+            FileCopyUtils.copy(productForm.getImage().getBytes(), new File(fileUpload + fileName));
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+        Product product = new Product(productForm.getId(), productForm.getName(), productForm.getDescription(), fileName);
+        productService.save(product);
+        ModelAndView modelAndView = new ModelAndView("/create");
+        modelAndView.addObject("productForm", productForm);
+        modelAndView.addObject("message", "Created new product successfully !");
+        return modelAndView;
+    }
+}
---
+++ src/main/java/cg/model/Product.java
@@ -0,0 +1,55 @@
+package cg.model;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "product")
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String name;
+    private double price;
+    private String description;
+    private String image;
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
